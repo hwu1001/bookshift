ede7ac58fb325998e43f03e5a25c804f6281133e
Henry Wu <hywu925@gmail.com>
2019-08-30 23:47:27 -0700

    refactor: Remove more unused styles

--- src/screens/MyShifts.tsx
+++ src/screens/MyShifts.tsx
@@ -15,13 +15,6 @@ const MyShifts: React.FC = () => {
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
   },
@@ -33,24 +26,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '600',
     color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  }
 });
 
 export default MyShifts;
