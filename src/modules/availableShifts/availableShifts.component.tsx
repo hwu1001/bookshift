ca85640e9eb2fc602c06558b29d55f7374c44795
Henry Wu <hywu925@gmail.com>
2019-09-17 09:13:44 -0700

    feat: Create Avail Shifts presentational component

---
+++ src/modules/availableShifts/availableShifts.component.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, ActivityIndicator, FlatList, Text } from 'react-native';
+import AreaFilterItem from '../../components/AreaFilterItem';
+import Shifts, { IShiftsProps } from '../core/shifts/Shifts.component';
+
+export interface ICityCount {
+  id: string,
+  area: string,
+  count: number
+}
+
+interface IAvailableShifts {
+  loading: boolean,
+  areaFilterProps: {
+    data: ReadonlyArray<ICityCount>,
+    filterCb: () => void
+  },
+  shiftListProps: IShiftsProps
+}
+
+const AvailableShifts: React.FC<IAvailableShifts> = (props: IAvailableShifts) => {
+  const { loading, areaFilterProps, shiftListProps } = props;
+
+  const _areaKeyExtractor = (item: ICityCount) => item.id;
+
+  return (
+    <SafeAreaView style={styles.body}>
+      {loading ?
+        <ActivityIndicator size="large" color="#0000ff" /> :
+        <>
+          <FlatList
+            data={areaFilterProps.data}
+            renderItem={({item}) => <AreaFilterItem area={item.area} orderCounter={item.count} filterCb={areaFilterProps.filterCb} />}
+            horizontal={true}
+            keyExtractor={_areaKeyExtractor}
+            contentContainerStyle={styles.areaFilter}
+          />
+          <Shifts
+            sections={shiftListProps.sections}
+          />
+        </>
+      }
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#ffffff',
+    flex: 1
+  },
+  areaFilter: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    flex: 1
+  }
+});
+
+export default AvailableShifts;
