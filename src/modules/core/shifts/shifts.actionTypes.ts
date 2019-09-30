5eda2440e5cc710c4ec84566e59c487a1ac86440
Henry Wu <hywu925@gmail.com>
2019-09-29 23:39:54 -0700

    feat: Add API and action types for book shift
    Left these files out of the last commit accidentally

--- src/modules/core/shifts/shifts.actionTypes.ts
+++ src/modules/core/shifts/shifts.actionTypes.ts
@@ -1,6 +1,9 @@
+// TODO - Rename these action types to be specific to fetching all shifts
 export const FETCH = 'shifts/FETCH';
 export const RECEIVED = 'shifts/RECEIVED';
-import { ShiftsState } from './shifts.stateTypes';
+export const BOOK = 'shifts/BOOK';
+export const BOOK_RECEIVED = 'shifts/BOOK_RECEIVED';
+import { ShiftsState, SingleShiftState } from './shifts.stateTypes';
 
 export interface FetchShiftsAction {
   type: typeof FETCH,
@@ -12,4 +15,14 @@ export interface ReceivedShiftsAction {
   payload: ShiftsState
 }
 
-export type ShiftActionTypes = FetchShiftsAction | ReceivedShiftsAction;
+export interface BookShiftAction {
+  type: typeof BOOK,
+  payload: SingleShiftState
+}
+
+export interface ReceivedBookShiftAction {
+  type: typeof BOOK_RECEIVED,
+  payload: SingleShiftState
+}
+
+export type ShiftActionTypes = FetchShiftsAction | ReceivedShiftsAction | BookShiftAction | ReceivedBookShiftAction;
--- src/modules/core/shifts/shifts.api.ts
+++ src/modules/core/shifts/shifts.api.ts
@@ -1,6 +1,13 @@
 import { baseUrl } from '../api';
 
+// TODO - Handle error from API
 export const fetchShifts = () => {
   return fetch(`${baseUrl}/shifts`)
   .then(response => response.json());
+};
+
+export const bookShift = (shiftId: string) => {
+  return fetch(`${baseUrl}/shifts/${shiftId}/book`, {
+    method: 'POST'
+  }).then(response => response.json());
 };
